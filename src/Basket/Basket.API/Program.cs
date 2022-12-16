7f82a424a53567fc87a9a0dca623bbf5cb3eabf6
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-12-16 22:58:33 +0100

    register services in DI container

--- src/Basket/Basket.API/Program.cs
+++ src/Basket/Basket.API/Program.cs
@@ -1,4 +1,7 @@
+using Basket.API.Data;
 using Basket.API.Data.Interfaces;
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IBasketContext, BasketContext>();
+builder.Services.AddTransient<IBasketRepository, BasketRepository>();
 
 ///Configure REDIS -------------------------------
 builder.Services.AddSingleton<ConnectionMultiplexer>(sp => 
