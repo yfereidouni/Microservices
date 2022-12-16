e57b4de09138eaf13fd7abd4072b323cd5af2aa5
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-12-16 23:14:47 +0100

    SwaggerDoc was configured.

--- src/Basket/Basket.API/Program.cs
+++ src/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@ using Basket.API.Data;
 using Basket.API.Data.Interfaces;
 using Basket.API.Repositories;
 using Basket.API.Repositories.Interfaces;
+using Microsoft.OpenApi.Models;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Version = "v1",
+        Description="Basket.API",
+        Contact = new OpenApiContact() 
+        {
+            Name="Yasser FEREIDOUNI",
+            Email="Yasser.Fereidouni@gmail.com",
+        }
+    });
+});
 
 builder.Services.AddTransient<IBasketContext, BasketContext>();
 builder.Services.AddTransient<IBasketRepository, BasketRepository>();
