7e21c2d84b5c5a98d665890dbba4948a6a69f933
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-12-18 17:30:18 +0100

    infrastructure and core layer was developed.

---
+++ src/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories.Base;
+
+namespace Ordering.Infrastructure.Repositories;
+
+public class OrderRepository : Repository<Order>, IOrderRepository
+{
+    public OrderRepository(OrderContext dbContext) 
+        : base(dbContext)
+    {
+    }
+
+    public async Task<IEnumerable<Order>> GetOrderByUserName(string userName)
+    {
+        var orderList = await _dbContext.Orders
+            .Where(o => o.UserName == userName) 
+            .ToListAsync();
+
+        return orderList;
+    }
+} 
