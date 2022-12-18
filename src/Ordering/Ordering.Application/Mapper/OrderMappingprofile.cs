5ac30611bf07177f461b3b75488262227644b356
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-12-18 21:19:43 +0100

    Implementing MediatR pattern.

---
+++ src/Ordering/Ordering.Application/Mapper/OrderMappingprofile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+
+namespace Ordering.Application.Mapper;
+
+public class OrderMappingprofile:Profile
+{
+	public OrderMappingprofile()
+	{
+		CreateMap<Order, OrderResponse>().ReverseMap();
+	}
+}
