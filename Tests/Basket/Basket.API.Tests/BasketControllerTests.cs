39d594f3615ff5a614ed1fd6c5500893efaf95ee
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-12-27 16:52:04 +0100

    BasketControllerTests was developed

---
+++ Tests/Basket/Basket.API.Tests/BasketControllerTests.cs
@@ -0,0 +1,11 @@
+namespace Basket.API.Tests
+{
+    [TestClass]
+    public class BasketControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
